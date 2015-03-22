9f9f52edfaab59d84f2a749598025a1a05fc7148
Guibert Timothé <timothe.guibert@outlook.fr>
2015-03-22 16:59:46 +0100

    re re re re test
    Azure c'est de la grosse merde.

--- server.js
+++ server.js
@@ -1,7 +1,47 @@
-var http = require('http');
-var app = require('app.js')
-var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+var sql = require('mssql'); 
+var express = require('express');
+var app = express();
+ 
+var config = {
+    user: 'culibase',
+    password: 'Culibdd123',
+    server: 'vh4omyemb2.database.windows.net',
+    database: 'culibase',
+    
+    options: {
+        encrypt: true // Use this if you're on Windows Azure 
+    }
+}
+
+var users = [
+  { name: 'tobi', email: 'tobi@learnboost.com' },
+  { name: 'loki', email: 'loki@learnboost.com' },
+  { name: 'jane', email: 'jane@learnboost.com' }
+];
+ 
+ app.engine('.html', require('ejs').__express);
+ app.set('views', __dirname + '\\views');
+ app.set('view engine', 'html');
+ 
+var connection = new sql.Connection(config, function(err) 
+{
+    var request = new sql.Request(connection); // or: var request = connection.request(); 
+    request.query('select * from test', function(err, recordset) 
+	{
+		app.get('/', function(req, res)
+		{
+			res.render('users', {
+			users: users,
+			title: recordset[0].test,
+			header: "Some users"
+			});
+		});
+    });
+});
+
+var server = app.listen(1337, function() {
+	var host = server.address().address
+	var port = server.address().port
+	
+	console.log('Example app listening at http://%s:%s', host, port)
+});
