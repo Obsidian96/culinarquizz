0fffe001f1410d9cff782dcf1faee94fe7ee80e4
Guibert Timothé <timothe.guibert@outlook.fr>
2015-03-22 16:40:57 +0100

    rereretest
    frwmt j

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var http = require('http');
-var app = require('./app')
+var app = require('app.js')
 var port = process.env.port || 1337;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
