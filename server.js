8e6abb608935e14bfac80058fa221586ab3abf4a
Guibert Timothé <timothe.guibert@outlook.fr>
2015-03-22 19:47:01 +0100

    Site completement fonctionnel en local
    Par contre pour azure...
    C'est mort.

--- server.js
+++ server.js
@@ -1,47 +1,38 @@
 var sql = require('mssql'); 
 var express = require('express');
 var app = express();
- 
-var config = {
-    user: 'culibase',
-    password: 'Culibdd123',
-    server: 'vh4omyemb2.database.windows.net',
-    database: 'culibase',
-    
-    options: {
-        encrypt: true // Use this if you're on Windows Azure 
-    }
-}
+var path = require('path');
 
-var users = [
-  { name: 'tobi', email: 'tobi@learnboost.com' },
-  { name: 'loki', email: 'loki@learnboost.com' },
-  { name: 'jane', email: 'jane@learnboost.com' }
-];
- 
- app.engine('.html', require('ejs').__express);
- app.set('views', __dirname + '\\views');
- app.set('view engine', 'html');
- 
-var connection = new sql.Connection(config, function(err) 
-{
+var config = {
+  user: 'culibase',
+  password: 'Culibdd123',
+  server: 'vh4omyemb2.database.windows.net',
+  database: 'culibase',
+
+  options: {
+        encrypt: true // Use this if you're on Windows Azure 
+      }
+    }
+
+    app.engine('.html', require('ejs').__express);
+    app.set('views', __dirname + '\\views');
+    app.set('view engine', 'html');
+
+    app.use(express.static(path.join(__dirname, 'public')));
+
+    var connection = new sql.Connection(config, function(err) 
+    {
     var request = new sql.Request(connection); // or: var request = connection.request(); 
     request.query('select * from test', function(err, recordset) 
-	{
-		app.get('/', function(req, res)
-		{
-			res.render('users', {
-			users: users,
-			title: recordset[0].test,
-			header: "Some users"
-			});
-		});
+    {
+      app.get('/', function(req, res)
+      {
+       res.render('index', {
+         title: recordset[0].test,
+         header: "Some users"
+       });
+     });
     });
-});
+  });
 
-var server = app.listen(1337, function() {
-	var host = server.address().address
-	var port = server.address().port
-	
-	console.log('Example app listening at http://%s:%s', host, port)
-});
+    app.listen(1337);
