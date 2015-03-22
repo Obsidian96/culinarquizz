7feb5e2c0fcf865c9019e418e9744a588880e2b2
Guibert Timothé <timothe.guibert@outlook.fr>
2015-03-22 14:30:45 +0100

    Test
    A hell lot of thing !

--- connect.js
+++ connect.js
@@ -12,30 +12,31 @@ var config = {
         encrypt: true // Use this if you're on Windows Azure 
     }
 }
+
+var users = [
+  { name: 'tobi', email: 'tobi@learnboost.com' },
+  { name: 'loki', email: 'loki@learnboost.com' },
+  { name: 'jane', email: 'jane@learnboost.com' }
+];
  
  app.engine('.html', require('ejs').__express);
- app.set('views', __dirname + '\views');
+ app.set('views', __dirname + '\\views');
  app.set('view engine', 'html');
  
-var connection = new sql.Connection(config, function(err) {
-    // ... error checks 
-    
-    // Query 
-    
+var connection = new sql.Connection(config, function(err) 
+{
     var request = new sql.Request(connection); // or: var request = connection.request(); 
-    request.query('select * from test', function(err, recordset) {
-        // ... error checks 
-        console.log(recordset[0].test);
-        console.dir(recordset);
+    request.query('select * from test', function(err, recordset) 
+	{
+		app.get('/', function(req, res)
+		{
+			res.render('users', {
+			users: users,
+			title: recordset[0].test,
+			header: "Some users"
+			});
+		});
     });
-    
-	app.get('/', function(req, res){
-    res.render('title', {
-    title: "EJS example",
-  });
-});
-
 });
 
 app.listen(80);
-console.log();
