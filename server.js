984703160e90fbdc7397ed175309c3be1cb22e7b
Guibert Timothé <timothe.guibert@outlook.fr>
2015-03-22 16:16:13 +0100

    Site ne marche pas,

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var sql = require('mssql'); 
 var express = require('express');
 var app = express();
+var http = require('http')
+var port = process.env.PORT || 1337;
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+})
  
 var config = {
     user: 'culibase',
@@ -40,10 +46,3 @@ var connection = new sql.Connection(config, function(err)
 });
 
 app.listen(1337);
-/*
-var http = require('http')
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);*/
